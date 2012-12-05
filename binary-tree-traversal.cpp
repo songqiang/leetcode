b51679e05bc56378edf9b55f77102095a4993b76
Song Qiang <qiang.song@usc.edu>
2012-12-05 12:18:04 -0800

    add level-order traversal of binary-tree using queues

--- binary-tree-traversal.cpp
+++ binary-tree-traversal.cpp
@@ -94,3 +94,149 @@ public:
 };
 
 
+/*
+
+Binary Tree Level Order Traversal
+
+Given a binary tree, return the level order traversal of its nodes'
+values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is
+serialized on OJ.
+
+ */
+
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        vector<vector<int> > r;
+        
+        std::queue<TreeNode*> q;
+        std::queue<size_t> level;
+
+        q.push(root);
+        level.push(0);
+
+        while (!q.empty())
+        {
+            if (q.front() == NULL)
+            {
+                q.pop();
+                level.pop();
+            }
+            else
+            {
+                q.push(q.front()->left);
+                level.push(level.front() + 1);
+
+                q.push(q.front()->right);
+                level.push(level.front() + 1);
+                
+                if (r.size() == level.front()) r.push_back(vector<int>());
+
+                r.back().push_back(q.front()->val);
+                q.pop();
+                level.pop();
+            }
+        }
+
+        return r;
+    }
+};
+
+/*
+
+Binary Tree Level Order Traversal II
+
+Given a binary tree, return the bottom-up level order traversal of its
+nodes' values. (ie, from left to right, level by level from leaf to
+root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7]
+  [9,20],
+  [3],
+]
+
+ */
+
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        vector<vector<int> > r;
+        
+        std::queue<TreeNode*> q;
+        std::queue<size_t> level;
+
+        q.push(root);
+        level.push(0);
+
+        while (!q.empty())
+        {
+            if (q.front() == NULL)
+            {
+                q.pop();
+                level.pop();
+            }
+            else
+            {
+                q.push(q.front()->left);
+                level.push(level.front() + 1);
+
+                q.push(q.front()->right);
+                level.push(level.front() + 1);
+                
+                if (r.size() == level.front()) r.push_back(vector<int>());
+
+                r.back().push_back(q.front()->val);
+                q.pop();
+                level.pop();
+            }
+        }
+
+        std::reverse(r.begin(), r.end());
+
+        return r;
+    }
+};
+
+
+
