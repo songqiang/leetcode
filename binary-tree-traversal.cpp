cb5a2815b3ae61fffc7db19b655e337eb5d8da08
Song Qiang <qiang.song@usc.edu>
2012-12-05 13:20:17 -0800

    add zig-zag level order traversal of binary tree

--- binary-tree-traversal.cpp
+++ binary-tree-traversal.cpp
@@ -239,4 +239,76 @@ public:
 };
 
 
+/*
+
+Binary Tree Zigzag Level Order Traversal
+
+Given a binary tree, return the zigzag level order traversal of its
+nodes' values. (ie, from left to right, then right to left for the
+next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is
+serialized on OJ.
+
+ */
+
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        vector<vector<int> > r;
+        
+        std::queue<TreeNode*> q;
+        std::queue<size_t> level;
+
+        q.push(root);
+        level.push(0);
+
+        while (!q.empty())
+        {
+            if (q.front() == NULL)
+            {
+                q.pop();
+                level.pop();
+            }
+            else
+            {
+                q.push(q.front()->left);
+                level.push(level.front() + 1);
+
+                q.push(q.front()->right);
+                level.push(level.front() + 1);
+                
+                if (r.size() == level.front()) r.push_back(vector<int>());
+
+                r.back().push_back(q.front()->val);
+                q.pop();
+                level.pop();
+            }
+        }
+
+        for (size_t i = 0; i < r.size(); ++i)
+            if (i % 2 == 1)
+                std::reverse(r[i].begin(), r[i].end());
+
+        return r;
+    }
+};
+
 
