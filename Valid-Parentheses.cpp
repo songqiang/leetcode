92f826b484f682d86f70d035c6dab4e2a483edaa
Song Qiang <qiang.song@usc.edu>
2014-01-02 18:50:11 -0800

    add a new solution with simpler code

--- Valid-Parentheses.cpp
+++ Valid-Parentheses.cpp
@@ -30,8 +30,35 @@ using namespace std;
 class Solution {
 public:
 
-
     bool isValid(string s) {
+        
+        stack<char> st;
+        for (size_t i = 0; i < s.size(); ++i)
+            switch (s[i])
+            {
+                case ')':
+                    if (st.empty()  || st.top() != '(')
+                        return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.empty()  || st.top() != '[')
+                        return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.empty()  || st.top() != '{')
+                        return false;
+                    st.pop();
+                    break;
+                default:
+                    st.push(s[i]);
+            }
+        return st.empty();      
+
+    }
+
+    bool isValid_bad_solution(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         
